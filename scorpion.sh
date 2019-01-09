2dac7c9d6eaebef335cf4abdf9601f9a4af2574d
Sushant Verma <vsushant8421@gmail.com>
2019-01-09 19:14:31 +0530

    Update scorpion.sh

--- scorpion.sh
+++ scorpion.sh
@@ -2,5 +2,5 @@
 
 git clone https://github.com/Untold-Destroyer/devilian_dt.git -b astro device/xiaomi/mido
 git clone https://github.com/Untold-Destroyer/vendor_xiaomi.git -b pie vendor/xiaomi
-git clone https://github.com/Untold-Destroyer/kernel_xiaomi_msm8953.git -b gzosp kernel/xioami/msm8953
+git clone https://github.com/Untold-Destroyer/kernel_xiaomi_msm8953.git -b gzosp kernel/xiaomi/msm8953
 git clone https://github.com/Untold-Destroyer/devilian_ct.git -b gzosp device/xiaomi/msm8953-common
